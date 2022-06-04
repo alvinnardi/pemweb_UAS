24356eb6965833a58590965fb38db2dfb46c334c
alvinnardi <alvinardi85@gmail.com>
2022-06-05 01:16:27 +0700

    ubah tampilan

--- resources/views/tentangKami/index.blade.php
+++ resources/views/tentangKami/index.blade.php
@@ -29,19 +29,21 @@
             <div class="col-12 col-lg-8 col-md-12"><br/>
             </div>     
             <div class="col-12 col-lg-4 col-md-12" ><br/>
-            <h1 class="judul" style="text-align:right"><font size="200%">Tentang Kami</font></h1><br/><br/>
-                <a><font size="5"><font size="6"><b>Restry</b></font> adalah aplikasi berbasis web yang berisikan kumpulan aneka resep makanan nusantara yang bertujuan untuk memudahkan pengguna mencari resep masakan
-                    Indonesia yang di inginkan. Kami menggunakan nama Restry dengan artian kata Res yaitu singkatan dari kata “resep”, dan Try  yang diambil dari kosa kata
-                    bahasa inggris yang berarti “coba”. jadi aplikasi ini mengajak pengguna untuk mencoba resep-resep makanan yang direkomendasikan pada website.<br/></font>
-                </a>
-            </div>     
+                <div style="margin: 0% 5% 0% 5%">
+                    <h1 class="judul" style="text-align:right"><font size="200%">Tentang Kami</font></h1><br/><br/>
+                    <a><font size="5"><font size="6"><b>Restry</b></font> adalah aplikasi berbasis web yang berisikan kumpulan aneka resep makanan nusantara yang bertujuan untuk memudahkan pengguna mencari resep masakan
+                        Indonesia yang di inginkan. Kami menggunakan nama Restry dengan artian kata Res yaitu singkatan dari kata “resep”, dan Try  yang diambil dari kosa kata
+                        bahasa inggris yang berarti “coba”. jadi aplikasi ini mengajak pengguna untuk mencoba resep-resep makanan yang direkomendasikan pada website.<br/></font>
+                    </a>
+                </div>
+            </div>   
             <div class="col-12 col-lg-3 col-md-12"><br/>
             </div>
             <div class="col-12 col-lg-4">
-                <div style="margin: 5% 5% 5% 5%">
+                <div style="margin: 0% 5% 0% 5%">
                     <h1 style="text-align:left;">Kontak Kami</h1>
                     <h6 style="text-align:left">Res.try@WAPFIVE.com<br/>Res.try.Facebook<br/>Res.try.Instagram<br/>©Copyright 2022</h6><br/><br/>
-                </div></div>
+                </div>
             </div>
         </div>  
     </div>
